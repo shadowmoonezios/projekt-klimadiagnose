49568f7c92c643096a3755d0026ecf4ff977fe75
shadowmoonezios <agushifelde@hotmail.com>
2025-09-07 22:35:59 +0300

    feat: initial commit for frontend/src/App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Projekt Klimadiagnose</h1>
+            <p>Willkommen zur Anwendung zur Erhebung und Analyse von Klimadaten!</p>
+        </div>
+    );
+}
+
+export default App;
